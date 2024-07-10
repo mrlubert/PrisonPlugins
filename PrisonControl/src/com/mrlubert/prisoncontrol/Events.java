7e7393aaf577cde29fbddc715b3ecde32abb2824
mrlubert <mrlubert@users.noreply.github.com>
2024-07-10 02:53:14 -0400

    Added Show Pick In Chat On Break

--- PrisonControl/src/com/mrlubert/prisoncontrol/Events.java
+++ PrisonControl/src/com/mrlubert/prisoncontrol/Events.java
@@ -13,6 +13,8 @@ import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.metadata.FixedMetadataValue;
+
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import java.util.ArrayList;
@@ -143,8 +145,12 @@ public class Events implements Listener
     public void onBlockPlace(BlockPlaceEvent event)
     {
     	Player player = event.getPlayer();
-        
+    	Material i = event.getBlockPlaced().getType();
         Block b = event.getBlock();
         b.setMetadata("blockBreaker", new FixedMetadataValue(PrisonPicks.getInstance(), player.getUniqueId()));
+        if (i == Material.SHULKER_BOX || i == Material.BLACK_SHULKER_BOX || i == Material.RED_SHULKER_BOX || i == Material.BLUE_SHULKER_BOX || i == Material.BROWN_SHULKER_BOX || i == Material.CYAN_SHULKER_BOX || i == Material.GRAY_SHULKER_BOX || i == Material.GREEN_SHULKER_BOX || i == Material.LIGHT_BLUE_SHULKER_BOX || i == Material.LIGHT_GRAY_SHULKER_BOX || i == Material.LIME_SHULKER_BOX || i == Material.MAGENTA_SHULKER_BOX || i == Material.ORANGE_SHULKER_BOX || i == Material.PINK_SHULKER_BOX || i == Material.PURPLE_SHULKER_BOX || i == Material.WHITE_SHULKER_BOX || i == Material.YELLOW_SHULKER_BOX) {
+        	player.sendMessage(ChatColor.DARK_PURPLE + "ShulkerBox: " + ChatColor.DARK_RED + "Shift + Drop Shulker To Open It");
+        	event.setCancelled(true);
+        }
         }
     }
